b1747eb1a6edaec8b3b927bd41fe2584c0dc28b3
chejianchao <820992376@qq.com>
2018-07-23 13:53:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,10 +9,10 @@ class Song
   def initialize
     @@songs << self
   end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  extend Findable
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def artist=(artist)
     @artist = artist
