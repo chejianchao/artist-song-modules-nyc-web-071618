d7d142459ae8b00cfbbe71c85c12c1186fc9b46f
chejianchao <820992376@qq.com>
2018-07-23 13:54:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,32 +1,28 @@
 require 'pry'
 
 class Song
+  
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
+  extend Findable
+  extend Memorable
+  
   def initialize
     @@songs << self
   end
-  extend Findable
   
   def artist=(artist)
     @artist = artist
   end
 
-  extend Memorable
-  # def self.reset_all
-  #   @@songs.clear
-  # end
-
+  
   def to_param
     name.downcase.gsub(' ', '-')
   end
 
-  # def self.count
-  #   self.all.count
-  # end
 
   def self.all
     @@songs
